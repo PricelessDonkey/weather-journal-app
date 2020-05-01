d5c9ffa7829ea41fbc511ea10a44c213900dbd72
Sam Drilias <sam@sams-air.attlocal.net>
2020-04-30 19:43:50 -0500

    submission one

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
+
+// Require Body-Parser to parse Json Bodies
+const bodyParser = require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+ function listening(){
+    console.log(`running on localhost: ${port}`);
+};
+  
+// GET route
+app.get('/all', sendData);
+
+function sendData (request, response) {
+  response.send(projectData);
+};
+
+// POST route
+app.post('/addData', addData);
+
+function addData(req, res) {
+
+    let newData = req.body;
+    let newEntry = {
+        temp: newData.temp,
+        date: newData.date,
+        feelings: newData.feelings
+    }
+    const size = projectData.push(newEntry);
+};
--- website/app.js
+++ website/app.js
@@ -1,5 +1,80 @@
 /* Global Variables */
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&units=imperial&appid=084900f4e5ae69045da58d41a51d4e39';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// store zip input in variable for quick access
+const zipInputElement = document.getElementById('zip')
+
+// listen for click on generate button
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+  clearErrorState();
+  e.preventDefault();
+
+  // get ZIP and user response from HTML
+  const zip = zipInputElement.value;
+  const userResponse = document.getElementById('feelings').value;
+  const testURL = baseURL + zip + apiKey;
+
+  getWeather(testURL)
+    .then(function (data) {
+      postData('/addData', { temp: data.main.temp, date: newDate, feelings: userResponse })
+        .then(updateUI())
+    }, function (error) {
+        setErrorState();
+    })
+};
+
+const getWeather = async (testURL) => {
+  
+  let res = await fetch(testURL);
+  if (res.status != 200) throw new Error('bad request'); 
+  try {   
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    }
+}
+
+// post data
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+}
+
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json();
+    const index = allData.length-1;
+   
+    document.getElementById('date').innerHTML = allData[index].date;
+    document.getElementById('temp').innerHTML = allData[index].temp + ' degrees fahrenheit';
+    document.getElementById('content').innerHTML = allData[index].feelings;
+
+    } catch (error) {
+      console.log("error", error);
+    }
+}
+
+function setErrorState() {
+  zipInputElement.classList.add('error');
+  zipInputElement.value = '';
+  zipInputElement.placeholder = 'invalid zip - try again';
+}
+
+function clearErrorState() {
+  zipInputElement.placeholder = 'enter zip code here';
+}
